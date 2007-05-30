49f66aee0b6dab00896f29c09bca0ea162d688b4
helder <helder@86e2ce6f-a531-0410-a1d1-23c94225625f>
2007-05-30 05:21:00 +0000

    lvebx quebrando. :-(

--- trunk/t/lvebx-1.c
+++ trunk/t/lvebx-1.c
@@ -2,10 +2,13 @@
 
 int main() {
 
+	uint8_t b00, b01, b02, b03, b04, b05, b06, b07, b08, b09, b10, b11, b12, b13, b14, b15;
+
 	uint8_t *uv = (int8_t *) 0x1000;
 	*uv = (int8_t) 42;
 
+	asm("lvebx 13, 0, %0" : : "r"(uv));
 
-	return 1;
-//	asm("lvebx 
+	STORE_VECTOR_UBYTE(13, b00, b01, b02, b03, b04, b05, b06, b07, b08, b09, b10, b11, b12, b13, b14, b15);
+	return !(b00 == 42);
 }
