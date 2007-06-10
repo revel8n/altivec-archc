d41dccc0805b41e154ead7f80d2e7597d75f0525
ribamar <ribamar@86e2ce6f-a531-0410-a1d1-23c94225625f>
2007-06-10 22:08:15 +0000

    vavgsw implemented and tested.

---
+++ t/vavgsw-1.c
@@ -0,0 +1,47 @@
+int main() {
+
+	/*vra.data[0] e vrb.data[0]*/
+	int *b = (int *) 2000;
+	int vec_a[] = {0x3015ffff, 0xffff3020, 0xff000c01,          0};
+	int vec_b[] = {0x1035dddd, 0xffff2030, 0x01000a01, 0xffffffff};
+
+	*b = vec_a[0];
+	asm( "li 2, 2000; "
+	    "lvewx 2, 0, 2; ");
+	*b = vec_b[0];
+	asm( "li 2, 2000; "
+	    "lvewx 3, 0, 2; ");
+
+	/*vra.data[1] e vrb.data[1]*/
+	b = (int *) 2004;
+	*b = vec_a[1];
+	asm( "li 2, 2004; "
+	    "lvewx 2, 0, 2; ");
+	*b = vec_b[1];
+	asm( "li 2, 2004; "
+	    "lvewx 3, 0, 2; ");
+
+	/*vra.data[2] e vrb.data[2]*/
+	b = (int *) 2008;
+	*b = vec_a[2];
+	asm( "li 2, 2008; "
+	    "lvewx 2, 0, 2; ");
+	*b = vec_b[2];
+	asm( "li 2, 2008; "
+	    "lvewx 3, 0, 2; ");
+
+	/*vra.data[3] e vrb.data[3]*/
+	b = (int *) 2012;
+	*b = vec_a[3];
+	asm( "li 2, 2012; "
+	    "lvewx 2, 0, 2; ");
+	*b = vec_b[3];
+	asm( "li 2, 2012; "
+	    "lvewx 3, 0, 2; ");
+
+
+	asm("vavgsw 1, 2, 3; ");
+
+
+	return *b;
+}
