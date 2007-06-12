157a55128ae37de50392cd4a70c063203c642fd9
cmarcelo <cmarcelo@86e2ce6f-a531-0410-a1d1-23c94225625f>
2007-06-12 03:24:47 +0000

    Adding more unsigned comparison instructions.
    vcmpequw_, vcmpequw, vcmpequh and vcmpequh_. However, I'm not
    checking if the flags at CR6 are being correctly set. :-/

---
+++ t/vcmpequh-1.c
@@ -0,0 +1,17 @@
+#include "test.h"
+
+// Check if unsigned halfword-wise equality works
+
+int main() {
+	uint32_t a, b, c, d;
+
+	LOAD_VECTOR_U(13, 0xFFFFFFFF, 0xFE10FF0F, 0xFFFF0000, 0xFFFFFFFF);
+	LOAD_VECTOR_U(14, 0xFFFFFF00, 0x0010FF0F, 0xFFFF0000, 0xFF0000FF);
+
+	asm("vcmpequh 15, 13, 14");
+
+	STORE_VECTOR_U(15, a, b, c, d);
+
+	return !(a == 0x00010000 && b == 0x00000001 &&
+		     c == 0x00010001 && d == 0x00000000);
+}
