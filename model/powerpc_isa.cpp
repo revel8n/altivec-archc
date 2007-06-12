f2e7abe239380b0f020fd7fa93a8202c6dff77a7
ribamar <ribamar@86e2ce6f-a531-0410-a1d1-23c94225625f>
2007-06-12 04:57:09 +0000

    vmsumubm implemented and tested.

--- model/powerpc_isa.cpp
+++ model/powerpc_isa.cpp
@@ -4850,10 +4850,80 @@ void ac_behavior( vmhraddshs ){}
 void ac_behavior( vmladduhm ){}
 
 //!Instruction vmsumubm behavior method.
-void ac_behavior( vmsumubm ){}
+// Vector Multiply-Sum Unsigned Byte Modulo - powerisa spec pag 164.
+void ac_behavior( vmsumubm ){
+
+    dbg_printf("vmsumubm v%d, v%d, v%d, v%d\n\n", vrt, vra, vrb, vrc);
+
+    vec t; 
+    vec a = VR.read(vra);
+    vec b = VR.read(vrb);
+    vec c = VR.read(vrc);
+
+    for (int i = 0; i < 4; i++){
+        int k = 8 ;  //  shifts de sizeof(BYTE). 
+        uint8_t a_i_0 = (uint8_t) ((a.data[i] << 3*k) >> 3*k); 
+        uint8_t a_i_1 = (uint8_t) ((a.data[i] << 2*k) >> 3*k); 
+        uint8_t a_i_2 = (uint8_t) ((a.data[i] << 1*k) >> 3*k); 
+        uint8_t a_i_3 = (uint8_t) ((a.data[i] << 0  ) >> 3*k); 
+        uint8_t b_i_0 = (uint8_t) ((b.data[i] << 3*k) >> 3*k); 
+        uint8_t b_i_1 = (uint8_t) ((b.data[i] << 2*k) >> 3*k); 
+        uint8_t b_i_2 = (uint8_t) ((b.data[i] << 1*k) >> 3*k); 
+        uint8_t b_i_3 = (uint8_t) ((b.data[i] << 0  ) >> 3*k); 
+        uint16_t t_0  = a_i_0 * b_i_0; 
+        uint16_t t_1  = a_i_1 * b_i_1; 
+        uint16_t t_2  = a_i_2 * b_i_2; 
+        uint16_t t_3  = a_i_3 * b_i_3; 
+        uint32_t c_i  = (uint32_t) c.data[i]; 
+        uint32_t t_i  =  t_0 + t_1 + t_2 + t_3 + c_i; 
+        t.data[i]     = (uint32_t)t_i; 
+        //debug information:
+        printf("(%X*%X);(%X*%X);(%X*%X);(%X*%X)\n",  
+                (unsigned char)a_i_3,  
+                (unsigned char)b_i_3, 
+                (unsigned char)a_i_2,  
+                (unsigned char)b_i_2, 
+                (unsigned char)a_i_1,  
+                (unsigned char)b_i_1, 
+                (unsigned char)a_i_0, 
+                (unsigned char)b_i_0); 
+
+        printf("(%lu*%lu);(%lu*%lu);(%lu*%lu);(%lu*%lu)\n",  
+                a_i_3,  
+                b_i_3, 
+                a_i_2,  
+                b_i_2, 
+                a_i_1,  
+                b_i_1, 
+                a_i_0, 
+                b_i_0); 
+        
+        printf(" t_3 + t_2 + t_1 + t_0 + ci ="
+                " %X +  %X +  %X +  %X + %X ="
+                "t_i  = %X \n",  
+                (unsigned short)t_3, 
+                (unsigned short)t_2, 
+                (unsigned short)t_1, 
+                (unsigned short)t_0, 
+                (unsigned int)  c_i, 
+                (unsigned int)  t_i); 
+
+        printf(" t_3 + t_2 + t_1 + t_0 + ci ="
+                " %lu +  %lu +  %lu + %lu + %lu ="
+                "t_i  = %lu \n",  
+                (short)t_3, 
+                (short)t_2, 
+                (short)t_1, 
+                (short)t_0, 
+                (int)  c_i, 
+                (int)  t_i); 
+        printf("\n"); 
+    }
+    VR.write(vrt, t); 
+}
 
 //!Instruction vmsummbm behavior method.
-// Vector Multiply-Sum Signed Halfword Modulo - powerisa spec pag 166.
+// Vector Multiply-Sum Mixed Byte Modulo - powerisa spec pag 164.
 void ac_behavior( vmsummbm ){
 
     dbg_printf("vmsummbm v%d, v%d, v%d, v%d\n\n", vrt, vra, vrb, vrc);
