3406a27423288f046916b0946cf1b6d07e09a3ce
helder <helder@86e2ce6f-a531-0410-a1d1-23c94225625f>
2007-06-13 03:12:33 +0000

    fixed test for vsl

--- t/vsl-1.c
+++ t/vsl-1.c
@@ -4,13 +4,13 @@ int main() {
 	uint32_t a, b, c, d;
 
 	LOAD_VECTOR_U(12, 0x01234567, 0x89ABCDEF, 0xFEDCBA98, 0x76543210);
-	LOAD_VECTOR_U(13, 0x00000004, 0x00000004, 0x00000004, 0x00000004);
+	LOAD_VECTOR_U(13, 0x04040404, 0x04040404, 0x04040404, 0x04040404);
 
 	asm("vsl 14, 12, 13");
 
 	STORE_VECTOR_U(14, a, b, c, d);
 
-	return !(a == 0xDEADBEEF && b == 0xDEADBEEF &&  
-		 c == 0xDEADBEEF && d == 0xDEADBEEF  );
+	return !(a == 0x12345670 && b == 0x9ABCDEF0 &&  
+		 c == 0xEDCBA988 && d == 0x6543210F  );
 }
 
