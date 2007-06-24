974751458752eb5e5d192f57ef4e73954dc77adc
ribamar <ribamar@86e2ce6f-a531-0410-a1d1-23c94225625f>
2007-06-24 22:17:56 +0000

    it seems impossible to work with (u)int64_t =/

--- trunk/t/vsro-1.c
+++ trunk/t/vsro-1.c
@@ -4,8 +4,8 @@ int main() {
 	uint32_t a, b, c, d;
 
 	LOAD_VECTOR_U(12, 0xa00ab00b, 0xc00cd00d, 0xe00ef00f, 0xc0aae0bb);
-	//0x0010-0000 first nibble = 3 bytes to shift right. 
-	LOAD_VECTOR_U(13, 0x00a00000, 0x00000000, 0x00000000, 0x00b00000);
+	//0x0000-0030 almost-last nibble of vrb = 3 bytes to shift right. 
+	LOAD_VECTOR_U(13, 0x00a00030, 0x00000000, 0x00000000, 0x00f000a0);
 	//LOAD_VECTOR_U(13, 0x00100000, 0x00000000, 0x00000000, 0x00000000);
 
 	asm("vsro 14, 12, 13");
