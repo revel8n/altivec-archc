9828790da238ceb985239f010ed3065fbdb1beb8
ribamar <ribamar@86e2ce6f-a531-0410-a1d1-23c94225625f>
2007-06-24 19:08:57 +0000

    test files for vspltb missed...

---
+++ trunk/t/vspltb-3.c
@@ -0,0 +1,20 @@
+#include "test.h"
+
+int main() {
+	uint8_t a, b, c, d, e;
+
+	LOAD_VECTOR_U(13, 0xaabbccdd, 0xeeffabcd, 0xacadaeaf, 0xbabcbdbe);
+
+	asm("vspltb 15, 13, 15");
+	// 0 ->  0xba; 
+	// 3 ->  0xbe; 
+	// 15 ->  0xdd; 
+	e = 0xdd; 
+
+	STORE_VECTOR_U(15, a, b, c, d);
+
+	return !(a == e &&
+             b == e &&
+             c == e &&
+             d == e);
+}
