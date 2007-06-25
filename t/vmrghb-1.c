47fc47e288edfab3af5789b8d20aafcf076e1bf8
ribamar <ribamar@86e2ce6f-a531-0410-a1d1-23c94225625f>
2007-06-25 05:20:35 +0000

    vmrghb implemented and tested.

---
+++ t/vmrghb-1.c
@@ -0,0 +1,16 @@
+#include "test.h"
+
+int main() {
+	uint32_t a, b, c, d;
+
+	LOAD_VECTOR_U(12, 0xa00ab00b, 0xc00cd00d, 0xe00ef00f, 0xc0aae0bb);
+	LOAD_VECTOR_U(13, 0x00a00030, 0x0f1111e0, 0x00000000, 0x00f000a0);
+
+	asm("vmrghb 14, 12, 13");
+
+	STORE_VECTOR_U(14, a, b, c, d);
+
+	return !(a == 0xf0000f00 && b == 0xe0000e00 &&  
+		 c == 0xe000bba0 && d == 0xc000aaf0);
+}
+
