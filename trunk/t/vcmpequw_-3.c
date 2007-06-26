8437510b22fd2fa61d215f2a5c2ed2b00a714670
cmarcelo <cmarcelo@86e2ce6f-a531-0410-a1d1-23c94225625f>
2007-06-26 02:55:16 +0000

    Tests for vcmpequw_

---
+++ trunk/t/vcmpequw_-3.c
@@ -0,0 +1,23 @@
+#include "test.h"
+#include "sample_data.h"
+
+int main() {
+	uint32_t a, b, c, d, branched;
+
+	LOAD_VECTOR_U(13, W1, W2, W3, W4);
+	LOAD_VECTOR_U(14, ~W1, ~W2, ~W3, ~W4);
+
+	asm("vcmpequw. 15, 13, 14");
+
+    DIE_IF_ALL_TRUE();
+    DIE_IF_NOT_ALL_FALSE();
+    
+	STORE_VECTOR_U(15, a, b, c, d);
+
+	if (!(a == 0x00000000 && b == 0x00000000 &&
+		  c == 0x00000000 && d == 0x00000000)) {
+        return 1;
+    }
+
+    return 0;
+}
