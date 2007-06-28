ecaca908dad62b4fec155e7a5af7c73c4f4f6c95
ribamar <ribamar@86e2ce6f-a531-0410-a1d1-23c94225625f>
2007-06-28 07:09:27 +0000

    vupklpx implemented and tested.

--- model/powerpc_isa.cpp
+++ model/powerpc_isa.cpp
@@ -4166,29 +4166,29 @@ void ac_behavior( vupkhpx ){
         uint8_t b1 = (hw0 >> 10) & 0x1f ; 
         uint8_t b2 = (hw0 >>  5) & 0x1f ; 
         uint8_t b3 = (hw0 >>  0) & 0x1f ; 
-        printf("hw0 = 0x%4x  hw0 & 0x8000 = 0x%4x\n", 
+        dbg_printf("hw0 = 0x%4x  hw0 & 0x8000 = 0x%4x\n", 
                 (unsigned short)hw0, (unsigned short)hw0 & 0x8000 ); 
-        printf("t = %#2x  %#2x  %#2x  %#2x\n ", (unsigned char) b0, 
+        dbg_printf("t = %#2x  %#2x  %#2x  %#2x\n ", (unsigned char) b0, 
                                       (unsigned char) b1, 
                                       (unsigned char) b2,
                                       (unsigned char) b3); 
 
         t.data[2*i] = (b0 << 24) + (b1 << 16) + (b2 << 8)  + b3; 
-        printf("t[%d] = 0x%8x\n\n", 2*i, (int)t.data[2*i]); 
+        dbg_printf("t[%d] = 0x%8x\n\n", 2*i, (int)t.data[2*i]); 
 
         b0 = (hw1 & 0x8000) ?  0xff : 0; 
         b1 = (hw1 >> 10) & 0x1f ; 
         b2 = (hw1 >>  5) & 0x1f ; 
         b3 = (hw1 >>  0) & 0x1f ; 
-        printf("hw1 = 0x%4x  hw1 & 0x8000 = 0x%4x\n", 
+        dbg_printf("hw1 = 0x%4x  hw1 & 0x8000 = 0x%4x\n", 
                 (unsigned short)hw1, (unsigned short)hw1 & 0x8000 ); 
-        printf("t = %#2x  %#2x  %#2x  %#2x\n ", (unsigned char) b0, 
+        dbg_printf("t = %#2x  %#2x  %#2x  %#2x\n ", (unsigned char) b0, 
                                       (unsigned char) b1, 
                                       (unsigned char) b2,
                                       (unsigned char) b3); 
 
         t.data[2*i+1] = (b0 << 24) + (b1 << 16) + (b2 << 8)  + b3; 
-        printf("t[%d] = 0x%8x\n\n", 2*i+1 , (int)t.data[2*i+1]); 
+        dbg_printf("t[%d] = 0x%8x\n\n", 2*i+1 , (int)t.data[2*i+1]); 
     }
     VR.write(vrt, t); 
 
@@ -4241,8 +4241,6 @@ void ac_behavior( vupkhsh ){
     vec t;
     vec b = VR.read(vrb);
     for (int i=0; i < 2; i++){
-        //int16_t hw0 = (int16_t) ((b.data[i] & 0xffff0000) >> 1*16); 
-        //int16_t hw1 = (int16_t) ((b.data[i] & 0x0000ffff) >> 0*16); 
         int16_t hw0 = (b.data[i] & 0xffff0000) >> 1*16; 
         int16_t hw1 = (b.data[i] & 0x0000ffff) >> 0*16; 
 
@@ -4267,7 +4265,45 @@ void ac_behavior( vupkhsh ){
 
 //!Instruction vupklpx behavior method.
 // Vector Unpack Low Pixel - powerisa spec pag 148. 
-void ac_behavior( vupklpx ){}
+void ac_behavior( vupklpx ){
+    dbg_printf(" vupklpx v%d, v%d\n\n", vrt, vrb);
+
+    vec t;
+    vec b = VR.read(vrb);
+    for (int i=0; i < 2; i++){
+        uint16_t hw0 = (b.data[i+2] & 0xffff0000) >> 1*16; 
+        uint16_t hw1 = (b.data[i+2] & 0x0000ffff) >> 0*16; 
+        uint8_t b0 = (hw0 & 0x8000) ?  0xff : 0; 
+        uint8_t b1 = (hw0 >> 10) & 0x1f ; 
+        uint8_t b2 = (hw0 >>  5) & 0x1f ; 
+        uint8_t b3 = (hw0 >>  0) & 0x1f ; 
+        printf("hw0 = 0x%4x  hw0 & 0x8000 = 0x%4x\n", 
+                (unsigned short)hw0, (unsigned short)hw0 & 0x8000 ); 
+        printf("t = %#2x  %#2x  %#2x  %#2x\n ", (unsigned char) b0, 
+                                      (unsigned char) b1, 
+                                      (unsigned char) b2,
+                                      (unsigned char) b3); 
+
+        t.data[2*i] = (b0 << 24) + (b1 << 16) + (b2 << 8)  + b3; 
+        printf("t[%d] = 0x%8x\n\n", 2*i, (int)t.data[2*i]); 
+
+        b0 = (hw1 & 0x8000) ?  0xff : 0; 
+        b1 = (hw1 >> 10) & 0x1f ; 
+        b2 = (hw1 >>  5) & 0x1f ; 
+        b3 = (hw1 >>  0) & 0x1f ; 
+        printf("hw1 = 0x%4x  hw1 & 0x8000 = 0x%4x\n", 
+                (unsigned short)hw1, (unsigned short)hw1 & 0x8000 ); 
+        printf("t = %#2x  %#2x  %#2x  %#2x\n ", (unsigned char) b0, 
+                                      (unsigned char) b1, 
+                                      (unsigned char) b2,
+                                      (unsigned char) b3); 
+
+        t.data[2*i+1] = (b0 << 24) + (b1 << 16) + (b2 << 8)  + b3; 
+        printf("t[%d] = 0x%8x\n\n", 2*i+1 , (int)t.data[2*i+1]); 
+    }
+    VR.write(vrt, t); 
+
+}
 
 //!Instruction vupklsb behavior method.
 // Vector Unpack Low Signed Byte - powerisa spec pag 148. 
