cd20539f86340e2d2cb83f176b60a774646c1224
cmarcelo <cmarcelo@86e2ce6f-a531-0410-a1d1-23c94225625f>
2007-07-01 05:54:58 +0000

    Fixing some dbg_printfs.

--- asm/teste.c
+++ asm/teste.c
@@ -1,22 +1,5 @@
 int main() {
-	register int a = 3;
-	int *b = (int *) 2000;
+    printf("haha\n");
 
-	*b = 42;
-
-	asm("li 3, 0; "
-	    "li 2, 2000; "
-	    "lvewx 1, 3, 2; ");
-
-	asm("li 3, 0; "
-	    "li 2, 3000; "
-	    "stvewx 1, 3, 2; " : : : "memory");
-
-	asm("li 3, 0;"
-	    "li 2, 3000; "
-	    "lvewx 2, 3, 2; ");
-
-	b = (int *) 3000;
-
-	return *b;
+	return 1;
 }
